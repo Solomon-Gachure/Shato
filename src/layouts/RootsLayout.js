ff9728b8819861a5e00c5782dfab2551a050531b
Solomon Gachure <gachuresolomon64@gmail.com>
2023-10-26 23:19:09 +0300

    Added some photos to the home page and some dependancies with it

--- src/layouts/RootsLayout.js
+++ src/layouts/RootsLayout.js
@@ -3,13 +3,13 @@ import { NavLink, Outlet } from 'react-router-dom'
 import {AiOutlineMenu} from 'react-icons/ai'
 const RootsLayout = () => {
   return (
-    <div className='max-w-[1400px] h-screen flex flex-col pt-2 mx-auto p-2  bg-cyan-950'>
+    <div className='max-w-[1400px] h-screen flex flex-col pt-2 mx-auto p-2  '>
         
         <header className='flex justify-between '>
             <div>
-                <AiOutlineMenu className='text-white' size={30}/>
+                <AiOutlineMenu className='' size={30}/>
             </div>
-            <h3 className='text-white'>BebaBeba</h3>
+            <h3 className='text-black'>BebaBeba</h3>
             <nav className='text-orange-500 hidden md:flex  gap-4  right-0 '>
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="about">About</NavLink>
@@ -24,6 +24,7 @@ const RootsLayout = () => {
         <main>
             <Outlet/>
         </main>
+        
     </div>
   )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,12 +2,31 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-white flex flex-col justify-center items-center' >
-       <img src="https://images.unsplash.com/photo-1509749837427-ac94a2553d0e?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGFzc2VuZ2VyfGVufDB8fDB8fHww" alt="mat" />
-        <h3>BebaBeba!!!</h3>
-        <h6>Welcome Aboard BebaBeba shuttle</h6>
-        <p>Take a drive around!</p>
+    <div className='max-w-[1400px] h-screen  pt-2 ' >
+       
+        <div className='flex flex-col justify-center items-center w-full p-4 py-8'>
+          <h3 className='text-3xl md:text-4xl'>BebaBeba!!!</h3>
+        <h6 className='text-xl flex-wrap p-4 md:text-6xl'>Welcome Aboard BebaBeba shuttle</h6>
+        <p className='text-lg md:text-4xl'>Take a drive around!</p>
         
+        </div>
+        <div className='grid md:grid-cols-3 gap-6 max-w-[1400px] h-screen'>
+          <div className='h-[550px] w-full border'>
+            <img className='h-[300px] w-full object-cover' src="https://media.istockphoto.com/id/1437821404/photo/3d-data-security-protection-icon-businessman-hand-holding-green-shield-floating-on-blue.jpg?s=612x612&w=0&k=20&c=ihK21NnGipCDLaPvxYLfnQl9M6Si8ubcTVDf-l5MwRc=" alt="safe drivers" />
+         <p className='p-2'><span className='border-b  pb-1 font-bold'>We offer safety through our highly trained drivers</span><br></br>
+         Our transport company prioritizes safety above all else. We are committed to providing a secure and reliable journey for our customers. Our vehicles undergo rigorous maintenance, our drivers are trained in safety protocols, and we adhere to strict safety regulations, ensuring peace of mind for all passengers.</p>
+          </div>
+          <div className='h-[550px] border w-full'>
+            <img className='h-[300px] w-full object-cover' src="https://media.istockphoto.com/id/1371650290/photo/3d-illustration-hands-giving-receiving-money.jpg?s=612x612&w=0&k=20&c=w5Ff0cnoIWjZ741UOv-6yBWMqqtTho1LnVhVnyV9hvQ=" alt="pocket friendly" />
+          <p className='p-2'><span className='border-b  pb-1 font-bold'>We will drive you nuts with our cheap prices</span><br></br>
+          Our transport company takes pride in offering budget-friendly rates. We believe in affordability without compromising quality. Through efficient operations and competitive pricing, we provide cost-effective transportation solutions that cater to our customers' diverse needs while ensuring a wallet-friendly experience for all.</p>
+          </div>
+          <div className='h-[550px] border'>
+            <img className='h-[300px] w-full object-cover' src="https://media.istockphoto.com/id/1486944463/photo/3d-rendering-of-clapping-hands.jpg?s=612x612&w=0&k=20&c=jAm8YCAQRAGKQ20q7clSwc9kiS41HmtI40nlPx41eNc=" alt="reliable" />
+          <p className='p-2'><span className='border-b  pb-1 font-bold'>We value customer satisfaction</span> <br></br>
+          Customer satisfaction is our top priority at our transport company. We go the extra mile to understand and fulfill our clients' needs. From personalized services to responsive support, we are dedicated to ensuring a seamless, enjoyable experience for every customer, fostering trust, loyalty, and lasting relationships. </p>
+          </div>
+        </div>
     </div>
   )
 }
