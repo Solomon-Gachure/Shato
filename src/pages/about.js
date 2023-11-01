80d6e5b4ed6132e3a52c9760ee6b71f62a51d543
Solomon Gachure <gachuresolomon64@gmail.com>
2023-11-01 20:24:56 +0300

    Added the contact page with different input fields where the customers can get to share their feedbacks, complaints and sentiments of the services offered, connected the input fields to the getform.io

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,10 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      
+      <div>
+        <h3>Know About Us:</h3>
+        <p></p>
+      </div>
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import {BsFacebook, BsInstagram, BsLinkedin, BsWhatsapp} from 'react-icons/bs'
+import {FaTiktok} from 'react-icons/fa'
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='max-w-[1400px] h-screen pt-2'>
+      <div className='w-full md:h-[500px] p-1 md:p-2'>
+        <img className='w-full object-cover md:h-full' src="https://media.istockphoto.com/id/1129113667/photo/got-a-problem-contact-us.webp?b=1&s=170667a&w=0&k=20&c=sS8SbvVpWJn2c9WrfbDMrTvM1TGpeO6HrpXogoUtLAY=" alt="shout out" />
+      </div>
+      {/**input fields + text area */}
+      <div className='flex flex-col justify-center items-center md:w-[1200px] p-4'>
+<h3>We Value your Feedback:</h3>
+<form action="" className='flex flex-col justify-center items-center md:w-[500px] gap-2 pt-2'>
+  <input type="text" placeholder='Enter Your Name' className='outline-none w-full border p-2' />
+  <input type="text" placeholder='Enter Your Email' className='outline-none w-full border p-2' />
+  <textarea name="message" id="" cols="30" rows="5" placeholder='Enter Your Message' className='w-full p-2 outline-none border'></textarea>
+</form>
+      </div>
+{/**Socials */}
+      <div className='sm:w-full flex flex-col justify-center items-center '>
+<p className=''>Follow us on all our socials to get updates and also comment on all your sentiments</p>
+<div className='flex justify-center items-center pt-2 gap-8 md:gap-20'>
+  <BsFacebook className=' cursor-pointer hover:scale-105 duration-500 ease-in' size={30}/>
+  <BsInstagram className='cursor-pointer hover:scale-105 duration-500 ease-in' size={30}/>
+  <BsWhatsapp className='cursor-pointer hover:scale-105 duration-500 ease-in' size={30}/>
+  <FaTiktok className='cursor-pointer hover:scale-105 duration-500 ease-in' size={30}/>
+  <BsLinkedin className='cursor-pointer hover:scale-105 duration-500 ease-in' size={30}/>
+
+</div>
+<div className='p-4'>
+  <p>Also shoot us an email, let us hear your concerns and feedback on our services to you as we value your sentiments.</p>
+</div>
+      </div>
+    </div>
   )
 }
 
